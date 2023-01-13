0a5d7551a6f48d3e2cf91886bf4ec6d61878b41a
elliot <73965218+vxgames@users.noreply.github.com>
2023-01-13 10:45:33 -0500

    Update script.js

--- sickness/script.js
+++ sickness/script.js
@@ -6,3 +6,14 @@ function virusClicked() {
   }
   document.getElementById("points").innerHTML = localStorage.points;
 }
+
+
+
+setInterval(function() {
+  if (localStorage.income) {
+    localStorage.points = Number(localStorage.points) + (Number(localStorage.income) / 50);
+  } else {
+    localStorage.income = 0;
+  }
+  document.getElementById("points").innerHTML = points;
+}, 20}
